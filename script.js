a9a6afb8268d81fb7c8ee5959d056826101e2281
HECTOR PAUL <hectorpaulc@gmail.com>
2019-06-24 14:07:07 -0500

    Actualizacion de drawList

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 createdObject((elm) => console.log("Created", elm))
 updateObject((elm) => console.log("Update", elm))
 deleteObject((elm) => console.log("Delete", elm))
+drawList([]) // Dibuja elementos a la lista en HTML
